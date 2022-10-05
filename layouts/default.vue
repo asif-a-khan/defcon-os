fc3b6dbdddd07ba131a03a0ac15a0547a6cc187e
asif-a-khan <asif14.k@hotmail.com>
2022-10-05 19:07:54 -0400

    footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
         <Nuxt />
       </v-container>
     </v-main>
-    <v-footer class="d-flex align-center justify-center py-2" color="black">
+    <v-footer class="d-flex align-center justify-center py-2" style="background-color: #121212;">
       <div class="d-flex flex-column align-center">
         <p class="text-caption my-0">Defcon OS &copy; 2022</p>
         <p class="text-caption mb-0 mt-1">Powered By Arch Linux</p>
